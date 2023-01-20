0381469116ae25bd67697e650770101188be38d7
Adam Chylaszek <adam.chylaszek@icloud.com>
2023-01-20 19:06:10 +0000

    Initial Commit

---
+++ HungryKaren/HungryKarenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungryKarenApp.swift
+//  HungryKaren
+//
+//  Created by Ewelina Klekot on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HungryKarenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
